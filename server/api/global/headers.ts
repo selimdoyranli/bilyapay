d133c6d1b5f333a320c7187b75c8c84590b4ffdb
selimdoyranli <selimdoyranli@gmail.com>
2026-01-05 21:45:59 +0300

    refactor: update headers

--- server/api/global/headers.ts
+++ server/api/global/headers.ts
@@ -1,8 +1,9 @@
 export const bilyonerHeaders = {
-  'User-Agent': 'PostmanRuntime/7.36.0',
+  'User-Agent': 'byp01',
   'Accept': '*/*',
   'Accept-Language': 'tr-TR,tr;q=0.9',
   'Connection': 'keep-alive',
   'Referer': 'https://www.bilyoner.com/',
-  'cookie': '123'
+  'Host': 'www.bilyoner.com',
+  'cookie': 'byp01'
 }
