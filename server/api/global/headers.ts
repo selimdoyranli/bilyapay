5acc9866c024324f5525978d05940f0ab05eb697
selimdoyranli <selimdoyranli@gmail.com>
2026-01-05 21:57:47 +0300

    refactor: update headers

--- server/api/global/headers.ts
+++ server/api/global/headers.ts
@@ -1,11 +1,9 @@
 export const bilyonerHeaders = {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
-  'Accept': 'application/json, text/plain, */*',
-  'Accept-Language': 'tr-TR,tr;q=0.9,en;q=0.8',
-  'Origin': 'https://bilyoner.com',
-  'Referer': 'https://bilyoner.com/',
-  'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="120"',
-  'cookie': 'byp01',
+  'User-Agent': 'byp01',
+  'Accept': '*/*',
+  'Accept-Language': 'tr-TR,tr;q=0.9',
   'Connection': 'keep-alive',
-  'Host': 'www.bilyoner.com'
+  'Referer': 'https://www.bilyoner.com/',
+  'Host': 'www.bilyoner.com',
+  'cookie': 'byp01'
 }
